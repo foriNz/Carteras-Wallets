ba7457554b270122f41cbfd13f8b6af78a855352
Mario <msumariva98@gmail.com>
2023-09-16 22:16:07 +0200

    XML creacion meta

---
+++ java/com/example/cartera_v1/Activities/CreacionMeta.java
@@ -0,0 +1,30 @@
+package com.example.cartera_v1.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import com.example.cartera_v1.R;
+
+public class CreacionMeta extends AppCompatActivity {
+    RadioButton rb_ingreso, rb_gasto, rb_ahorro;
+    EditText objetivo;
+    Spinner sp_categorias;
+    Button btn_aceptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion_meta);
+        rb_ahorro = findViewById(R.id.rb_categoria_ahorro_meta);
+        rb_gasto = findViewById(R.id.rb_categoria_gasto_meta);
+        rb_ingreso = findViewById(R.id.rb_categoria_ingreso_meta);
+        objetivo = findViewById(R.id.et_objetivo_meta);
+        sp_categorias = findViewById(R.id.sp_categorias);
+        btn_aceptar = findViewById(R.id.btn_aceptar_meta);
+    }
+}
